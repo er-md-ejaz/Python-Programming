5bb559c8fd76e754fac21460eec66fd040e4542a
MD Ejaz Ansari <ejazansari0175@gmail.com>
2025-05-07 20:02:07 +0530

    Delete chapter_2/chapter_2_exercise_3.py

--- chapter_2/chapter_2_exercise_3.py
+++
@@ -1,5 +0,0 @@
-name, char = input("Enter your name and character: ").split(",")
-# print(f"Length of name is: {len(name)}, and no. of {char} is {name.lower().count(char.lower())}")
-
-
-print(f"Length of name is: {len(name)} and no. of {char} is {name.lower().strip().count(char.lower().strip())}")
