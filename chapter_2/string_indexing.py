a72fd5630a3f682df56828c9e10a74ce75ef18eb
MD Ejaz Ansari <ejazansari0175@gmail.com>
2025-05-07 20:11:49 +0530

    Add files via upload

---
+++ chapter_2/string_indexing.py
@@ -0,0 +1,18 @@
+name = "Ejaz Ansari"
+
+# position or index number
+# E = 0, -11
+# j = 1, -10
+# a = 2, -9
+# z = 3, -8
+#   = 4, -7
+# A = 5, -6
+# n = 6, -5
+# s = 7, -4
+# a = 8, -3
+# r = 9, -2 
+# i = 10, -1
+
+print(name[5])
+print(name[4])
+print(name[-1])
