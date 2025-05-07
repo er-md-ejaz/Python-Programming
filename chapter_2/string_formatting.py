2d1475176018c7b1b747388e0e8e9149a40d3f69
MD Ejaz Ansari <ejazansari0175@gmail.com>
2025-05-07 20:04:44 +0530

    Delete chapter_2/string_formatting.py

--- chapter_2/string_formatting.py
+++
@@ -1,6 +0,0 @@
-name, age = "Ejaz", 12
-print("your name: " + name + " your age: " + str(age)) # conventional method
-
-print("your name: {} your age: {}".format(name, age)) # python 3
-print(f"your name: {name} your age: {age}") #python 3.6 //// be careful while writing formatting using python 3.6 we need to put
-# f before first double quote 
