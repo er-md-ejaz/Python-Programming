e1a167572c6bfb551cd53c868e1314271fdf906a
MD Ejaz Ansari <ejazansari0175@gmail.com>
2025-05-07 20:09:21 +0530

    Add files via upload

---
+++ chapter_2/string_formatting.py
@@ -0,0 +1,6 @@
+name, age = "Ejaz", 12
+print("your name: " + name + " your age: " + str(age)) # conventional method
+
+print("your name: {} your age: {}".format(name, age)) # python 3
+print(f"your name: {name} your age: {age}") #python 3.6 //// be careful while writing formatting using python 3.6 we need to put
+# f before first double quote 
