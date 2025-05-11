5993c5646a62aea9004120508ca332f26d32809b
MD Ejaz Ansari <ejazansari0175@gmail.com>
2025-05-11 19:20:33 +0530

    Add files via upload

---
+++ chapter_3/conditionals/and_or_operator.py
@@ -0,0 +1,13 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+# if name == "ejaz" and age == 19:
+#     print("condition true")
+# else:
+#     print("condition false")
+
+
+if name == "ejaz" or age == 19:
+    print("condition true")
+else:
+    print("condition false")
