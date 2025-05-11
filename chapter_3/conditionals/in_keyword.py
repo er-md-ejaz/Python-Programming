ebdf71783ba5c434be244e647f0d833bd49bdbc2
MD Ejaz Ansari <ejazansari0175@gmail.com>
2025-05-11 19:19:19 +0530

    Add files via upload

---
+++ chapter_3/conditionals/in_keyword.py
@@ -0,0 +1,12 @@
+# to check a character is present in a string or not
+
+name = "md ejaz ansari"
+# if 's' in name:
+#     print("s is present")
+# else:
+#     print("s is not present")
+
+if 'c' in "md ejaz ansari":
+    print("c is present")
+else:
+    print("c is not present")
