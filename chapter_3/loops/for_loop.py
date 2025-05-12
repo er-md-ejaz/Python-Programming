a8ae6773c33266e2163ded3420c882c03cedf163
MD Ejaz Ansari <ejazansari0175@gmail.com>
2025-05-12 19:44:34 +0530

    Add files via upload

---
+++ chapter_3/loops/for_loop.py
@@ -0,0 +1,17 @@
+# for loop with range function
+
+# for i in range(10): # i=0 to 9
+#     print(f"hello world {i}")
+
+# for i in range(1, 10): # i=1 to 9
+#     print(f"hello world {i}")
+
+
+# step argument in range function
+
+# for i in range(1, 10, 2): # i=1 to 9 with step of 2
+#     print(f"hello world {i}")
+
+# print 10 9 8...1
+for i in range(10, 0, -1):
+    print(i)
