ab1f2180508bdab72956656eab380136a3ee9572
MD Ejaz Ansari <ejazansari0175@gmail.com>
2025-05-12 19:45:25 +0530

    Add files via upload

---
+++ chapter_3/loops/for_loop_string.py
@@ -0,0 +1,16 @@
+for i in range(1, 11):
+    print(i)
+
+name = "md ejaz ansari"
+for i in name:
+    print(i)
+
+for i in "md ejaz ansari":
+    print(i)
+
+# sum of number
+num = input("Enter a number: ")
+sum = 0
+for i in num:
+    sum += int(i)
+print(sum)
