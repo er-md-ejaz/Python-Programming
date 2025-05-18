4fc889f901ae1358aca8ad7b6be1ef4c32a65c70
MD Ejaz Ansari <ejazansari0175@gmail.com>
2025-05-18 11:34:21 +0530

    Add files via upload

---
+++ chapter_3/loops/break_continue.py
@@ -0,0 +1,13 @@
+# break keyword -> if we want to come out from any loop then we can use break keyword
+
+# for i in range(1, 11):
+#     if i==5:
+#         break
+#     print(i)
+
+# continue keyword -> print 1 to 10 except 5
+
+for i in range(1, 11):
+    if i==5:
+        continue
+    print(i)
