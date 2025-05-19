b10ef6e21e6046f956971d0ebc120be1b79ee0f9
MD Ejaz Ansari <ejazansari0175@gmail.com>
2025-05-20 00:43:22 +0530

    Create function_intro.py

---
+++ chapter_4/function_intro.py
@@ -0,0 +1,10 @@
+def add_two_argument(a, b):
+    return a + b
+
+# x = int(input("Enter first argument: "))
+# y = int(input("Enter second argument: "))
+# print(add_two_argument(x, y))
+
+first_name = input("Enter first name: ")
+last_name = input("Enter second name: ")
+print(add_two_argument(first_name, last_name))
