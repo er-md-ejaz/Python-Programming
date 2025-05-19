f36c84526aabcf3ce42737476b0da463ccf35d2a
MD Ejaz Ansari <ejazansari0175@gmail.com>
2025-05-20 00:44:49 +0530

    Add files via upload

---
+++ chapter_4/print_vs_return.py
@@ -0,0 +1,6 @@
+def add_three(a, b, c):
+    # return a+b+c  # it'll only return the value, it can't be print
+    print(a + b + c) # it print the addition we can use according to our question
+
+
+add_three(4,5,3)
