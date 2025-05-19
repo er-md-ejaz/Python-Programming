cbcbb9a05f2cc7d2fdff3c82226ae01b9a5ffc8c
MD Ejaz Ansari <ejazansari0175@gmail.com>
2025-05-20 00:43:50 +0530

    Add files via upload

---
+++ chapter_4/function_practice.py
@@ -0,0 +1,33 @@
+# print last character of a string 
+
+# def print_last_char(name):
+#     return name[-1]
+
+# name = input("Enter your name: ")
+# print(print_last_char(name))
+
+
+# find odd and even
+ 
+# def odd_even(n):
+#     if n%2 == 0:
+#         return "even" # we can use print() also instead of return
+#     else:
+#         return "odd"
+
+# def odd_even(n):
+#     if n%2 == 0:
+#         return "even" # we can use print() also instead of return
+#     return "odd"
+
+# print(odd_even(5))
+
+# def is_even(n):
+#     if n%2 == 0:
+#         return True
+#     return False
+
+def is_even(n):
+    return n%2 == 0
+
+print(is_even(7))
