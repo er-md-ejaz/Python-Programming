9e3b9d44bc245a77b6d0f4ff808627b341ad3e36
MD Ejaz Ansari <ejazansari0175@gmail.com>
2025-05-23 22:24:39 +0530

    Add files via upload

---
+++ chapter_5/append_data.py
@@ -0,0 +1,36 @@
+# how to add items to our list at last position
+
+# fruits = ['apple', "mango"]
+# print(fruits)
+
+# fruits.append("grapes")
+# print(fruits)
+
+
+#! some other method to add data to our list
+# insert method
+
+fruits1 = ['mango', 'orange']
+# fruits1.insert(1, 'grapes')
+# print(fruits1)
+# fruits1.insert(25, "apple")
+# print(fruits1)
+# fruits1.insert(-1, "pineapple")
+# print(fruits1)
+
+
+
+# how to join(concatenate) two list 
+fruits2 = ["grapes", "pineapple"]
+# fruits = fruits1 + fruits2
+# print(fruits)
+
+
+# extend method
+fruits1.extend(fruits2)
+print(fruits1)
+print(fruits2)
+
+
+# difference between append and extend method
+# append add whole list into an list(list inside a list) as a data
