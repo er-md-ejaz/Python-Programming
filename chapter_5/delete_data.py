96985ba410bb2a915591dfd651b86e4e75f95b13
MD Ejaz Ansari <ejazansari0175@gmail.com>
2025-05-23 22:25:05 +0530

    Add files via upload

---
+++ chapter_5/delete_data.py
@@ -0,0 +1,22 @@
+fruits = ["apple", 'grapes', 'mango', "pineapple", "apple", "orange"]
+print(fruits)
+# fruits.pop()
+# print(fruits)
+# fruits.pop(2)
+# print(fruits)
+# del fruits[1]
+# print(fruits)
+
+# remove method -> if we don't know the index of any element and want to remove that element then we can
+# use remove method
+# fruits.remove('mango')
+# print(fruits)
+# fruits.remove("banana")
+# print(fruits)
+
+
+fruits.remove("apple")  # it'll remove only first apple not all
+print(fruits)
+
+#! for add data to list we use append, insert, extend
+#! for remove pop, remove, del
